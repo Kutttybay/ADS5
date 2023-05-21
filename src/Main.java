a9e9a924b067049bb408129f4345398457a80bb3
kutttybay <kutttybay@gmail.com>
2023-05-21 23:54:26 +0600

    3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello in Assignment 5");
+        BST<Integer, String> bst = new BST<>();
+
+        for (int i = 0; i < 20; i++){
+            bst.put(i, "Value " + i);
+        }
     }
 }
