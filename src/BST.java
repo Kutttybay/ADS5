b718200af2fdc1997ebaefda117bad73b6299537
kutttybay <kutttybay@gmail.com>
2023-05-22 00:41:05 +0600

    8

--- src/BST.java
+++ src/BST.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class BST<K extends Comparable<K>,V> {
+
+    // Вложенный класс Node представляет узел дерева. Он содержит ключ, значение и ссылки на левого и правого потомков.
     private Node root;
     public class Node{
         private K key;
@@ -25,7 +27,13 @@ public class BST<K extends Comparable<K>,V> {
     public int getSize(){
         return size;
     }
+    /*
+    *  метод рекурсивно вставляет новый узел в дерево. Если узел равен null, создается новый узел с указанным ключом и значением.
+    *  Если ключ меньше текущего узла, происходит рекурсивный вызов для левого поддерева, иначе для правого.
+    *  Если ключ уже существует, обновляется значение.
+    */
     private Node insertNode(Node node, K key, V value){
+
         if (node == null){
             return new Node(key, value);
         }
@@ -38,11 +46,16 @@ public class BST<K extends Comparable<K>,V> {
         }
         return node;
     }
+    // метод вставляет новый узел в дерево и увеличивает размер дерева.
     public void put(K key,V value){
         this.root = insertNode(root, key, value);
         size++;
     }
 
+    /*
+    * метод рекурсивно находит узел с указанным ключом в дереве. Если текущий узел равен null или ключи совпадают, возвращается текущий узел.
+    * В противном случае происходит рекурсивный вызов для соответствующего поддерева.
+    */
     private Node getTreeNode(Node node, K key){
         if (root != null || node.key.equals(key)){
             return node;
@@ -54,11 +67,21 @@ public class BST<K extends Comparable<K>,V> {
             return getTreeNode(node.right, key);
         }
     }
+
+    // метод возвращает значение, связанное с указанным ключом, или null, если ключ не найден.
     public V get(K key){
         Node node = getTreeNode(root,key);
         return (node.equals(null) ? null : node.value);
     }
 
+    /*
+    * метод рекурсивно удаляет узел с указанным ключом из дерева. Если текущий узел равен null, возвращается null.
+    *  Если ключ меньше текущего узла, происходит рекурсивный вызов для левого поддерева, иначе для правого.
+    *  Если найден узел для удаления, происходит проверка его потомков. Если узел является листом, он удаляется.
+    *  Если у узла отсутствует левый или правый потомок, соответствующая ссылка заменяется на существующего потомка.
+    *  Если у узла есть оба потомка, находится минимальный узел в правом поддереве и его ключ и значение копируются в текущий узел.
+    *  Затем происходит рекурсивное удаление минимального узла из правого поддерева.
+    */
     private Node deleteNode(Node node, K key){
         if (node == null){
             return null;
@@ -85,11 +108,13 @@ public class BST<K extends Comparable<K>,V> {
         return node;
     }
 
+    // метод удаляет узел с указанным ключом из дерева и уменьшает размер дерева.
     public void delete(K key){
         this.root = deleteNode(root, key);
         size--;
     }
 
+    // метод находит узел с минимальным ключом в дереве путем итеративного спуска влево.
     private Node findMinimumNode(Node node){
         while (node.left != null){
             node = node.left;
@@ -97,6 +122,10 @@ public class BST<K extends Comparable<K>,V> {
         return node;
     }
 
+    /*
+    * метод выполняет обход дерева в порядке "in-order" (левый потомок, узел, правый потомок) и добавляет узлы в переданный список list.
+    *  Метод также рекурсивно вызывает сам себя для левого и правого поддеревьев.
+    */
     private ArrayList<Node> inOrderTraversal(ArrayList list, Node node){
         if (node == null){
             return null;
@@ -110,6 +139,10 @@ public class BST<K extends Comparable<K>,V> {
         }
         return list;
     }
+    /*
+    * метод возвращает итератор для обхода элементов дерева в порядке "in-order".
+    * Он вызывает inOrderTraversal для получения списка узлов, который затем приводится к типу Iterable и возвращается.
+    */
     public Iterable<Node> iterator(){
         ArrayList<Node> arr = inOrderTraversal(new ArrayList<>(), root);
         return (Iterable) arr;
