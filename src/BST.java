70d246434efbf11c36e24e3488377b0c35e3c200
kutttybay <kutttybay@gmail.com>
2023-05-21 23:47:50 +0600

    1

---
+++ src/BST.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+public class BST<K extends Comparable<K>,V> {
+    private Node root;
+    public class Node{
+        private K key;
+        private V value;
+        private Node left, right;
+        public Node(K key, V value){
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "key=" + key +
+                    ", value=" + value +
+                    '}';
+        }
+    }
+    private int size = 0;
+
+
+}
